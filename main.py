aa35641968f7be3b07c45944648b98de2d5aa137
Felipe Villegas <felavid@gmail.com>
2021-06-24 23:15:54 -0400

    all tests OK

--- main.py
+++ main.py
@@ -3,7 +3,18 @@ from time_calculator import add_time
 from unittest import main
 
 
-print(add_time("11:06 PM", "2:02"))
+# print("Test #1 expects 5:42 PM\n", add_time("3:30 PM", "2:12"), "\n")
+# print("Test #2 expects 3:07 PM \n", add_time("11:55 AM", "3:12"), "\n")
+# print("Test #3 expects 2:45 AM (next day)\n", add_time("9:15 PM", "5:30"), "\n")
+# print("Test #4 expects 12:05 PM\n", add_time("11:40 AM", "0:25"), "\n")
+# print("Test #5 expects 2:59 AM (next day)\n", add_time("2:59 AM", "24:00"), "\n")
+# print("Test #6 expects 12:04 AM (2 days later)\n", add_time("11:59 PM", "24:05"), "\n")
+# print("Test #7 expects 6:18 AM (20 days later)\n", add_time("8:16 PM", "466:02"), "\n")
+# print("Test #8 expects 5:01 AM\n", add_time("5:01 AM", "0:00"), "\n")
+# print("Test #9 expects 5:42 PM, Monday\n", add_time("3:30 PM", "2:12", "Monday"), "\n")
+# print("Test #10 expects 2:59 AM, Sunday (next day)\n", add_time("2:59 AM", "24:00", "saturDay"), "\n")
+# print("Test #11 expects 12:04 AM, Friday (2 days later)\n", add_time("11:59 PM", "24:05", "Wednesday"), "\n")
+# print("Test #12 expects 6:18 AM, Monday (20 days later)\n", add_time("8:16 PM", "466:02", "tuesday"), "\n")
 
 
 # Run unit tests automatically
--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,97 @@
-def add_time(start, duration):
+def add_time(start, duration, *day):
+    # print(start, duration)
+    new_time = ''
 
+    # Start arg breakdown
+    start_time = start.split()
+    start_hhmm = start_time[0].split(':')
+    start_hh = int(start_hhmm[0])
+    start_mm = int(start_hhmm[1])
+    meridian = [start_time[1]]
+    if meridian[0] == 'AM':
+        meridian.append('PM')
+    elif meridian[0] == 'PM':
+        meridian.append('AM')
 
+    # Duration arg breakdown
+    add_hhmm = duration.split(':')
+    add_hh = int(add_hhmm[0])
+    add_mm = int(add_hhmm[1])
 
+    # Optional day arg
+    week_day = ''
+    if day:
+        week_day = day[0].title()
 
+    # Calculations
+    start_minutes = (start_hh * 60) + start_mm
+    add_minutes = (add_hh * 60) + add_mm
+    days_count = int(add_minutes / 1440)
+    result_minutes = start_minutes + add_minutes
+
+    result_hh = int(result_minutes / 60)
+    result_mm = result_minutes % 60
+
+    # Output builder
+    meridian_index = int(int(result_hh / 12) % 2)
+    final_mm = str(result_mm)
+
+    if len(final_mm) < 2:
+        final_mm = '0' + final_mm
+
+    while result_hh > 12:
+        result_hh -= 12
+
+    final_meridian = meridian[meridian_index]
+    final_hh = str(result_hh)
+
+    final_days = ''
+    week_day_count = 0
+
+    if meridian[0] == 'PM' and final_meridian == 'AM':
+        if days_count < 1:
+            final_days = final_days + '(next day)'
+            week_day_count += 1
+        elif days_count >= 1:
+            final_days = final_days + f'({days_count + 1} days later)'
+            week_day_count += days_count + 1
+    elif meridian[0] == 'AM' and final_meridian == 'AM':
+        if days_count == 1 or (days_count < 1 and add_minutes > 720):
+            final_days = final_days + '(next day)'
+            week_day_count += 1
+        elif days_count > 1:
+            final_days = final_days + f'({days_count} days later)'
+            week_day_count += days_count
+    elif meridian[0] == 'PM' and final_meridian == 'PM':
+        if days_count == 1 or (days_count < 1 and add_minutes > 720):
+            final_days = final_days + '(next day)'
+            week_day_count += 1
+        elif days_count > 1:
+            final_days = final_days + f'({days_count} days later)'
+            week_day_count += days_count
+    elif meridian[0] == 'AM' and final_meridian == 'PM':
+        if days_count < 1 and add_minutes > 720:
+            final_days = final_days + '(next day)'
+            week_day_count += 1
+        elif days_count >= 1:
+            final_days = final_days + f'({days_count + 1} days later)'
+            week_day_count += days_count + 1
+
+    week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+    if week_day:
+        day_index = week.index(week_day)
+        rotated_week = week[day_index:] + week[:day_index]
+        week_day = rotated_week[week_day_count % 7]
+
+    # Return constructor
+    new_time = new_time + final_hh + ':' + final_mm + ' ' + final_meridian
+
+    if week_day and final_days:
+        new_time = new_time + ', ' + week_day + ' ' + final_days
+    elif final_days:
+        new_time = new_time + ' ' + final_days
+    elif week_day:
+        new_time = new_time + ', ' + week_day
 
     return new_time
